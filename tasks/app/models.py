f45511cbc67abb540f76eac467990421f0ce1bf8
Abhinav <kumar.abhinav3125@gmail.com>
2024-01-09 20:30:25 +0530

    Fetched the JSON file programmatically and stored the data in a MySQL database.

--- tasks/app/models.py
+++ tasks/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Products(models.Model):
+    subcategory = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    price = models.IntegerField()
+    popularity = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
--- tasks/app/views.py
+++ tasks/app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+from .models import Products
+from .serializers import ProductsSerializer
 
 # Create your views here.
+
+
+@api_view(['GET', ])
+def fetch_json(request):
+    # Fetch data from the external API
+    url = 'https://s3.amazonaws.com/open-to-cors/assignment.json'
+    response = requests.get(url)
+    data = response.json()
+    data = data["products"]
+
+    data_list = [value for value in data.values()]
+
+    for item_data in data_list:
+        serializer = ProductsSerializer(data = item_data)
+        if serializer.is_valid():
+            serializer.save()
+        else :
+            print(serializer.errors)
+
+    return Response({"message": "Data saved successfully"}, status=status.HTTP_201_CREATED)
