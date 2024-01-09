13f1760d6f28a6dd6b5bec76f12f7713548c8e80
Abhinav <kumar.abhinav3125@gmail.com>
2024-01-10 02:20:56 +0530

    Completed objective 1: Fetched data from JSON, stored it in a database and displayed to the user, ordered by decreasing popularity.

--- tasks/app/app_urls.py
+++ tasks/app/app_urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('fetch_json/', fetch_json),
-    
+    path('json_data_api/', json_data),
+    path('show_json_data/', show_json_data),    
 ]
--- tasks/app/views.py
+++ tasks/app/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,9 +10,9 @@ from .serializers import ProductsSerializer
 
 # Create your views here.
 
-
-@api_view(['GET', ])
+# function to fetch data from json and save in the database.
 def fetch_json(request):
+    print('WORKING')
     # Fetch data from the external API
     url = 'https://s3.amazonaws.com/open-to-cors/assignment.json'
     response = requests.get(url)
@@ -19,10 +22,29 @@ def fetch_json(request):
     data_list = [value for value in data.values()]
 
     for item_data in data_list:
-        serializer = ProductsSerializer(data = item_data)
-        if serializer.is_valid():
-            serializer.save()
-        else :
-            print(serializer.errors)
 
-    return Response({"message": "Data saved successfully"}, status=status.HTTP_201_CREATED)
+        if not Products.objects.filter(subcategory = item_data['subcategory'], title = item_data['title'], price = item_data['price'], popularity = item_data['popularity']).exists():
+            serializer = ProductsSerializer(data = item_data)
+
+            if serializer.is_valid():
+                serializer.save()
+            else :
+                print(serializer.errors)
+
+    return HttpResponse("Data saved successfully!!!")
+
+
+# Locally hosted API to be used to fetch the JSON data in frontend.
+@api_view(['GET', ])
+def json_data(request) :
+    if request.method == 'GET' :
+        tasks = Products.objects.all()
+        serialized = ProductsSerializer(tasks, many = True)
+        # sort the data based on popularity in descending order.
+        sorted_data = sorted(serialized.data, key=lambda x: x.get('popularity', 0), reverse=True)
+        return Response(sorted_data)
+
+
+# function to display json data to users.
+def show_json_data(request) :
+    return render(request, 'load_json_data.html')
